c983f82864cdde2737c1bc81dc1f87c2b0189528
jectan <jerico.tan03@gmail.com>
2025-03-19 00:04:06 +0800

    2025-03-19 00:00 Commit

---
+++ app/Http/Controllers/ReviewController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\DocType;
+use App\Models\RequestDocument;
+use App\Models\ReviewDocument;
+use App\Models\RequestType;
+use Illuminate\Support\Facades\DB;
+
+use Illuminate\Http\Request;
+
+class ReviewController extends Controller
+{
+
+    public function __construct(SeriesService $series_service) 
+    {
+        $this->series_service = $series_service;
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+        'requestID' => 'required|integer|exists:requestID',
+        'reviewComment' => 'required|string|max:500',
+        ]);
+
+        if($request->reviewStatus == 'Approved')
+        {
+            $requestStatus = 'For Approval';
+        }
+        else
+        {
+            $requestStatus = 'For Revision';
+        }
+
+        RequestDocument::where('requestID', $request->requestID)
+            ->update(['requestStatus' => $requestStatus]);
+
+        $reviewDocument = ReviewDocument::updateOrCreate(['reviewID' => $request->reviewID],
+        [
+            'requestID' => $request->requestID,
+            'reviewComment' => $request->reviewComment,
+            'userID' => Auth::id(),
+            'reviewDate' => Carbon::now(),
+            'reviewStatus' => $request->reviewStatus,                  
+        ]);
+                        
+        return response()->json(['success'=> 'Successfully saved.', 'ReviewDocument' => $reviewDocument]);
+    }
+}
