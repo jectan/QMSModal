36a6b32232d84dd3db8f06f9b5f4cdbc3cb18fa6
jectan <jerico.tan03@gmail.com>
2025-04-12 01:08:32 +0800

    2025-04-11 23:00 Commit

--- app/Http/Controllers/AboutController.php
+++ app/Http/Controllers/AboutController.php
@@ -8,7 +8,7 @@ class AboutController extends Controller
 {
     public function index()
     {
-        $page = 'About'; // for active nav highlighting
+        $page = 'About'; // for active nav highlightings
         return view('pages.aboutus.index', compact('page'));
     }
 }
